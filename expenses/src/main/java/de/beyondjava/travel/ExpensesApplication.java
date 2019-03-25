5e226427e67c1fbbe8a9888c62f16b9c0abc0d19
stephan <webmaster1@beyondjava.de>
2019-03-25 21:27:04 +0100

    refactoring

--- expenses/src/main/java/de/beyondjava/travel/ExpensesApplication.java
+++ expenses/src/main/java/de/beyondjava/travel/ExpensesApplication.java
@@ -1,4 +1,4 @@
-package de.beyondjava.travel.expenses;
+package de.beyondjava.travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- expenses/src/main/java/de/beyondjava/travel/expenses/ExpenseRepository.java
+++ expenses/src/main/java/de/beyondjava/travel/expenses/ExpenseRepository.java
@@ -1,13 +1,26 @@
+package de.beyondjava.travel.expenses;
+
 import java.util.List;
 
 import de.beyondjava.travel.expenses.Expense;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@RepositoryRestResource(collectionResourceRel = "expense", path = "expense")
 public interface ExpenseRepository extends PagingAndSortingRepository<Expense, Long> {
 
-    List<Expense> findByLastName(@Param("name") String name);
+    Expense save(Expense expense);
+
+    List<Expense> findByHotel(@Param("hotel") String hotel);
+
+    List<Expense> findAll();
+
+    List<Expense> findAll(Sort var1);
+
+    Page<Expense> findAll(Pageable var1);
 
 }
