6007725fbc87952c31b1c4c08d2f82c23d5c09e6
Corine Mbobda <Corine.Mbobda@opitz-consulting.com>
2019-05-29 17:25:21 +0200

    added the post and patch method

--- expenses/src/main/angular/app/expenses.service.ts
+++ expenses/src/main/angular/app/expenses.service.ts
@@ -12,15 +12,15 @@ export class ExpensesService {
 
   public getHotels(): Observable<Array<Expense>> {
     if (this.listOfExpenses.length === 0) {
-    return this.http.get('http://localhost:5000/expenses')
-      .pipe(
-         // tap(x => console.log(x)),
-        map((response: any) => response._embedded.expenses),
-        // tap(y => console.log(y)),
-        map((expenses: Array<any>) => expenses.map(expenseElement => this.extractId(expenseElement))),
-         // tap(exp => console.log(exp)),
-        tap(exp => this.listOfExpenses = exp)
-      );
+      return this.http.get('http://localhost:5000/expenses')
+        .pipe(
+           // tap(x => console.log(x)),
+          map((response: any) => response._embedded.expenses),
+          // tap(y => console.log(y)),
+          map((expenses: Array<any>) => expenses.map(expenseElement => this.extractId(expenseElement))),
+           // tap(exp => console.log(exp)),
+          tap(exp => this.listOfExpenses = exp)
+        );
     } else {
       return of(this.listOfExpenses);
     }
@@ -39,22 +39,55 @@ export class ExpensesService {
     };
   }
 
+  public postHotel(postExpense: { id: number; hotel: string; amount: number }): void {
+    const body = postExpense;
+    this.http.post('http://localhost:5000/expenses', body ).subscribe(
+      (x) => {
+        console.log(x); // x already contains the new id --> we can optimize the remaining code
+        this.listOfExpenses = []; // force the reload
+        this.getHotels().subscribe(); // reload list of hotels
+      },
+      error => console.log(error)
+    );
+  }
+
+  public patch(patchExpense: { id: number; hotel: string; amount: number }): void {
+    const body = patchExpense;
+    this.http.patch('http://localhost:5000/expenses' + '/' + patchExpense.id, body )
+
+      .subscribe(
+      (x) => {
+        console.log(x); // x already contains the new id --> we can optimize the remaining code
+        this.listOfExpenses = []; // force the reload
+        this.getHotels().subscribe(); // reload list of hotels
+      },
+      error => console.log(error)
+    );
+}
+
   public save(expense: { id: number; hotel: string; amount: number }): void {
     if (expense.id) {
-      this.currentExpense.hotel = expense.hotel;
-      this.currentExpense.amount = expense.amount;
-    } else {
-      if (this.listOfExpenses.length === 0) {
-        expense.id = 1;
-      } else {
-        const ids = this.listOfExpenses.map(e => e.id);
+      // this.currentExpense.hotel = expense.hotel;
+      // this.currentExpense.amount = expense.amount;
 
-        const newId = 1 + Math.max(...ids);
-        expense.id = newId;
-      }
-      this.listOfExpenses.push(expense);
+      this.patch(expense);
+    } else {
+      // if (this.listOfExpenses.length === 0) {
+      //   expense.id = 1;
+      // } else {
+      //   const ids = this.listOfExpenses.map(e => e.id);
+      //
+      //   const newId = 1 + Math.max(...ids);
+      //   expense.id = newId;
+      // }
+      // this.listOfExpenses.push(expense);
+
+      this.postHotel(expense);
+
     }
     console.log(expense);
+
+
   }
 
   editItem(itemExpense: Expense): void {
